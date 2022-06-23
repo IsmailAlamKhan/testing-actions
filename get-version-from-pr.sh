5954ab001d6d5d4313d13cc8b2bc43ae64fca77f
IsmailAlamKhan <67656229+IsmailAlamKhan@users.noreply.github.com>
2022-06-23 20:33:10 +0600

    Update get-version-from-pr.sh

--- get-version-from-pr.sh
+++ get-version-from-pr.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 title=$1
+echo "title is $title"
 regex='v([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,2})\+([0-9]{1,2})'
 matched=''
 [[ $title =~ $regex ]] && matched="${BASH_REMATCH}"
+echo "matched: $matched"
 echo "::set-output name=matched::$(echo $matched)"
