1187f45d5898b58ebaf242b39111310dd3d1b0bb
IsmailAlamKhan <67656229+IsmailAlamKhan@users.noreply.github.com>
2022-06-23 20:17:56 +0600

    Update get-version-from-pr.sh

--- get-version-from-pr.sh
+++ get-version-from-pr.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 regex='v([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,2})\+([0-9]{1,2})'
 matched=''
-[[ ${{ github.event.pull_request.title }} =~ $regex ]] && matched="${BASH_REMATCH}"
+[[ $pr_title =~ $regex ]] && matched="${BASH_REMATCH}"
 echo "::set-output name=version::$matched"
