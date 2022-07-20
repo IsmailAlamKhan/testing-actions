5c7cc27f0d5b0c1530b705638d1c142e8235aa8f
IsmailAlamKhan <67656229+IsmailAlamKhan@users.noreply.github.com>
2022-07-20 16:29:36 +0600

    Update get-version-from-pr.sh

--- get-version-from-pr.sh
+++ get-version-from-pr.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-title=$1
-echo "title is $title"
-regex='v([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,2})\+([0-9]{1,2})'
-version=''
-[[ $title =~ $regex ]] && version="${BASH_REMATCH}"
-echo "version: $version"
-echo "::set-output name=version::$(echo $version)"
+version=$1
+version=`cat version.txt`
+echo "::set-output name=version::$(echo $version| tr -d '[:space:]')"
