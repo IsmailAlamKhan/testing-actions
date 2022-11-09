95ea1b18c6d1adba2e75b1864e690168e27dbf70
IsmailAlamKhan <mdismailalamkhan@gmail.com>
2022-11-10 02:52:45 +0600

    ssad

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 response=$(curl https://deto-events.com/v1/version)
-echo "::set-output name=version::$(echo response from curl= $response)"
+# echo "::set-output name=version::$(echo response from curl= $response)"
+
+echo "response=$response" >> $GITHUB_OUTPUT
+
